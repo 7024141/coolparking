9dc6e068d630a7cbcacd86777bfc898121b4767a
7024141 <872327141@qq.com>
2019-07-03 10:49:05 +0800

    Initial commit

---
+++ src/main/java/com/example/coolparking/CoolparkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.coolparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolparkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolparkingApplication.class, args);
+    }
+
+}
